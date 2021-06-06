e78e18054d4478aa6ca3f0680b226c87058da595
Eleven <qiuqiujun666@gmail.com>
2021-06-06 12:58:19 +0800

    feat: 添加 PLUGIN_METADATA 储存插件信息

---
+++ dimension_convert.py
@@ -0,0 +1,11 @@
+PLUGIN_METADATA = {
+    'id': 'dimension_convert.py',
+    'version': '1.0.0',
+    'name': 'Dimension Convert',
+    'description': 'A plugin to convert dimension coordinates.',
+    'author': 'eleven',
+    'link': 'https://github.com/ActiniumCraft/dimension-convert',
+    'dependencies': {
+        'mcdreforged': '>=1.0.0'
+    }
+}
