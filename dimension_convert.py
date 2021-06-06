1db9f4415e11f998c05442b61b0147c9539b542e
Eleven <qiuqiujun666@gmail.com>
2021-06-06 18:11:53 -0500

    feat: 实现插件主功能
    根据用户输入坐标返回对应维度坐标

--- dimension_convert.py
+++ dimension_convert.py
@@ -17,9 +17,33 @@ HELP_MESSAGE = '''
 转换主世界和地狱的对应坐标，快速搭建地狱交通。
 Github: https://github.com/ActiniumCraft/dimension-convert
 !!cdc 显示帮助信息
-!!cdc overworld x y z 将主世界坐标转为地狱坐标
-!!cdc nether x y z 将地狱坐标转为主世界坐标
+!!cdc overworld x z 将主世界坐标转为地狱坐标
+!!cdc nether x z 将地狱坐标转为主世界坐标
 '''
+
+
+def is_args_length_match(args: list, length: int):
+    return len(args) == length
+
+
+def on_info(server, info):
+    user_args: list = info.content.split(' ')
+
+    if not user_args[0] == '!!cdc':
+        return
+
+    if is_args_length_match(user_args, 1):
+        server.reply(info, HELP_MESSAGE)
+
+    if is_args_length_match(user_args, 4):
+        x_coordinate = float(user_args[2])
+        z_coordinate = float(user_args[3])
+        converted_coordinate = [0, 0]
+        if user_args[1] == 'nether':
+            converted_coordinate = [int(x_coordinate*8), int(z_coordinate*8)]
+        if user_args[1] == 'overworld':
+            converted_coordinate = [int(x_coordinate/8), int(z_coordinate/8)]
+        server.reply(info, '对应维度坐标: x = {}, z = {}'.format(converted_coordinate[0], converted_coordinate[1]))
 
 
 def on_load(server, prev):
