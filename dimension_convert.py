319aeb11b58ac00474c30d075ee852d1507d1b04
Eleven <qiuqiujun666@gmail.com>
2021-06-08 13:27:53 -0500

    refactor: 新方法，新变量，添加方法说明
    - 坐标转换方法 convert_dimension_coordinate
    - 方法 is_args_length_match 的说明
    - 助于代码理解的新变量

--- dimension_convert.py
+++ dimension_convert.py
@@ -22,28 +22,49 @@ HELP_MESSAGE = '''
 '''
 
 
-def is_args_length_match(args: list, length: int):
+def is_args_length_match(args: list[str], length: int) -> bool:
+    """Compare the args list by giving length.
+
+    :param args: The args list. For example: ['!!cdc', 'nether', '8', '8'].
+    :param length: The args expects to be. For example: 4 match giving args list ['!!cdc', 'nether', '8', '8'].
+    :return: The bool value. True when args length match; otherwise, it's False.
+    """
     return len(args) == length
+
+
+def convert_dimension_coordinate(dimension: str, x_coord: float, z_coord: float) -> dict[str, int]:
+    """Convert nether/overworld dimension coordinate to its opposites dimension.
+
+    :param dimension: The dimension name, value args are 'nether' and 'overworld'.
+    :param x_coord: The x coordinate used in converting.
+    :param z_coord: The z coordinate used in converting.
+    :return: A dict[str, int] store the coordinate information, keys are x and z.
+    """
+    coordinate = {'x': 0, 'z': 0}
+    if dimension == 'nether':
+        coordinate = {'x': int(x_coord * 8), 'z': int(z_coord * 8)}
+    if dimension == 'overworld':
+        coordinate = {'x': int(x_coord / 8), 'z': int(z_coord / 8)}
+    return coordinate
 
 
 def on_user_info(server: ServerInterface, info: Info):
     user_args = info.content.split(' ')
+    command_prefix = user_args[0]
 
-    if not user_args[0] == '!!cdc':
+    if not command_prefix == '!!cdc':
         return
 
     if is_args_length_match(user_args, 1):
         server.reply(info, HELP_MESSAGE)
 
     if is_args_length_match(user_args, 4):
+        dimension = user_args[1]
         x_coordinate = float(user_args[2])
         z_coordinate = float(user_args[3])
-        converted_coordinate = [0, 0]
-        if user_args[1] == 'nether':
-            converted_coordinate = [int(x_coordinate * 8), int(z_coordinate * 8)]
-        if user_args[1] == 'overworld':
-            converted_coordinate = [int(x_coordinate / 8), int(z_coordinate / 8)]
-        server.reply(info, '对应维度坐标: x = {}, z = {}'.format(converted_coordinate[0], converted_coordinate[1]))
+
+        converted_coordinate = convert_dimension_coordinate(dimension, x_coordinate, z_coordinate)
+        server.reply(info, '对应维度坐标: x = {}, z = {}'.format(converted_coordinate['x'], converted_coordinate['z']))
 
 
 def on_load(server, prev):
