c9dbeb0041b3f841d5221c113fa3e4193aa51289
Eleven <qiuqiujun666@gmail.com>
2021-06-08 17:59:24 -0500

    feat: 当前坐标转换为对应维度坐标

--- dimension_convert.py
+++ dimension_convert.py
@@ -1,4 +1,4 @@
-from mcdreforged.api.types import ServerInterface, Info
+from mcdreforged.api.all import *
 
 PLUGIN_METADATA = {
     'id': 'dimension_convert',
@@ -8,7 +8,8 @@ PLUGIN_METADATA = {
     'author': 'eleven',
     'link': 'https://github.com/ActiniumCraft/dimension-convert',
     'dependencies': {
-        'mcdreforged': '>=1.0.0'
+        'mcdreforged': '>=1.0.0',
+        'minecraft_data_api': '*'
     }
 }
 
@@ -17,6 +18,7 @@ HELP_MESSAGE = '''
 §l转换主世界和地狱的对应坐标，快速搭建地狱交通。§r
 §6Github: https://github.com/ActiniumCraft/dimension-convert§r
 §7!!cdc§r 显示帮助信息
+§7!!cdc here§r 将玩家当前所在坐标转换为对应坐标
 §7!!cdc overworld x z§r 将主世界坐标转为地狱坐标
 §7!!cdc nether x z§r 将地狱坐标转为主世界坐标
 '''
@@ -48,6 +50,27 @@ def convert_dimension_coordinate(dimension: str, x_coord: float, z_coord: float)
     return coordinate
 
 
+@new_thread(PLUGIN_METADATA['name'])
+def execute_convert_command_by_player_coordinate(source: Info) -> None:
+    """Execute the dimension convert command by giving player command source info.
+
+    :param source: The command source info.
+    :return: None.
+    """
+    api = source.get_server().get_plugin_instance('minecraft_data_api')
+    coordinate = api.get_player_coordinate(source.player)
+    dim_convert_dict = {0: 'overworld', -1: 'nether'}
+    dimension = dim_convert_dict[api.get_player_dimension(source.player)]
+    converted_coordinate = convert_dimension_coordinate(dimension, coordinate.x, coordinate.z)
+    source.get_server().reply(source, '对应维度坐标: x = {}, z = {}'.format(converted_coordinate['x'],
+                                                                      converted_coordinate['z']))
+
+
+def convert_if_command_is_here(user_args, info) -> None:
+    if user_args[1] == 'here':
+        execute_convert_command_by_player_coordinate(info)
+
+
 def on_user_info(server: ServerInterface, info: Info):
     user_args = info.content.split(' ')
     command_prefix = user_args[0]
@@ -57,6 +80,9 @@ def on_user_info(server: ServerInterface, info: Info):
 
     if is_args_length_match(user_args, 1):
         server.reply(info, HELP_MESSAGE)
+
+    if is_args_length_match(user_args, 2):
+        convert_if_command_is_here(user_args, info)
 
     if is_args_length_match(user_args, 4):
         dimension = user_args[1]
